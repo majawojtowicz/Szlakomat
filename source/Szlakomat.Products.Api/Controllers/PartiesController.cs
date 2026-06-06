1d2100b603a5eaa9d7b51567a002e6f4b9017280
maja.wojtowicz <wojtowiczmagdalenamaja@gmail.com>
2026-06-06 11:11:52 +0200

    small modifications

---
+++ source/Szlakomat.Products.Api/Controllers/PartiesController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Szlakomat.Parties.Application.RegisterPerson;
+using Szlakomat.Parties.Application.FindAllParties;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PartiesController(IMediator mediator) : ControllerBase
+{
+    [HttpPost("person")]
+    public async Task<IActionResult> RegisterPerson([FromBody] RegisterPerson cmd)
+        => Ok(await mediator.Send(cmd));
+
+    [HttpGet]
+    public async Task<IActionResult> All()
+        => Ok(await mediator.Send(new FindAllPartiesCriteria()));
+}
--- source/Szlakomat.Products.Api/Program.cs
+++ source/Szlakomat.Products.Api/Program.cs
@@ -1,8 +1,10 @@
 using Szlakomat.Products.Infrastructure;
+using Szlakomat.Parties.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddProductModule();
+builder.Services.AddPartyModule();
 
 var app = builder.Build();
 app.MapControllers();
