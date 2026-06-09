62b8ba9c54337ea0731e1d06ae6fa63a436b28cf
maja.wojtowicz <wojtowiczmagdalenamaja@gmail.com>
2026-06-09 09:37:53 +0200

    additional tests

--- source/Szlakomat.Parties.Application.Tests/PartyScenariosTests.cs
+++ source/Szlakomat.Parties.Application.Tests/PartyScenariosTests.cs
@@ -8,6 +8,8 @@ using Szlakomat.Parties.Application.RegisterJdg;
 using Szlakomat.Parties.Application.RegisterPerson;
 using Szlakomat.Parties.Application.Relationships.AssignRelationship;
 using Szlakomat.Parties.Infrastructure;
+using Szlakomat.Parties.Application.Relationships.FindActiveRelationships;
+using Szlakomat.Parties.Application.FindPartyById;
 
 public class PartyScenariosTests
 {
@@ -72,5 +74,46 @@ public class PartyScenariosTests
             "VISITED"));
 
         Assert.True(rel.IsSuccess());
+    }
+
+    [Fact]
+    public async Task Active_relationships_only_include_non_terminated()
+    {
+        var jan = await _mediator.Send(new RegisterPerson("Jan", "K",
+            ["VISITOR"], []));
+        var tatromania = await _mediator.Send(new RegisterJdg("Anna", "N",
+            "Tatromania", "1234567890", ["ATTRACTION_OWNER"], []));
+
+        await _mediator.Send(new AssignPartyRelationship(
+            jan.SuccessValue, "VISITOR",
+            tatromania.SuccessValue, "ATTRACTION_OWNER",
+            "VISITED"));
+
+        var active = await _mediator.Send(
+            new FindActiveRelationshipsCriteria(jan.SuccessValue));
+
+        Assert.Single(active);
+        Assert.Equal("VISITED", active[0].Name);
+    }
+
+    [Fact]
+    public async Task Jdg_is_natural_person_with_business_identity()
+    {
+        var marek = await _mediator.Send(new RegisterJdg(
+            "Marek", "Wisniewski",
+            "Przewodnik Gorski Wisniewski Marek",
+            "7771234567",
+            ["TOUR_GUIDE", "ATTRACTION_OWNER"],
+            [new IdentifierInput("NIP", "7771234567"),
+         new IdentifierInput("PESEL", "79031412345")]));
+
+        var view = await _mediator.Send(
+            new FindPartyByIdCriteria(marek.SuccessValue));
+
+        Assert.NotNull(view);
+        Assert.Equal("JDG", view.PartyType);
+        Assert.True(view.IsNaturalPerson);
+        Assert.Contains(view.Identifiers, i => i.Type == "NIP");
+        Assert.Contains(view.Identifiers, i => i.Type == "PESEL");
     }
 }
